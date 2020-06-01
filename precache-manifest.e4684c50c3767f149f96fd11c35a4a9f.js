fbf3cbce119ec37781d5da1b1e56a4767a486808
irinaswofford <irinaswofford@gmail.com>
2020-06-01 17:16:02 -0500

    Updates

---
+++ precache-manifest.e4684c50c3767f149f96fd11c35a4a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4092e3240ac5fd968ca072cefb4c66",
+    "url": "/manage-file/index.html"
+  },
+  {
+    "revision": "dc1c892ce0b1c099c33e",
+    "url": "/manage-file/static/css/main.609df5c1.chunk.css"
+  },
+  {
+    "revision": "eafd85e03cb9dc6ce881",
+    "url": "/manage-file/static/js/2.ac016bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manage-file/static/js/2.ac016bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1c892ce0b1c099c33e",
+    "url": "/manage-file/static/js/main.d31da486.chunk.js"
+  },
+  {
+    "revision": "bca1d6e1d43e656b89fe",
+    "url": "/manage-file/static/js/runtime-main.69db604a.js"
+  }
+]);
